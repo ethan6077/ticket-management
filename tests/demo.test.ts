bf4d120bda3a23e9bcdfe7ac38b180891cffdd4d
Ethan Zhang <ethan6077@gmail.com>
2021-03-11 07:57:14 +1100

    feat: updated demo test

--- tests/demo.test.ts
+++ tests/demo.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai'
 
-describe('calculate', function() {
-  it('add', function() {
+describe('calculate', () => {
+  it('add', () => {
     const result = 5 + 2;
     expect(result).equal(7);
   }); 
